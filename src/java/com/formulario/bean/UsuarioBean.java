50c0ca2c46866eb50d892fa965628957329f0179
praseo <pacocamas85@gmail.com>
2018-11-23 10:30:48 +0100

    Tercer commit: Navegacion funcionando

--- src/java/com/formulario/bean/UsuarioBean.java
+++ src/java/com/formulario/bean/UsuarioBean.java
@@ -2,8 +2,11 @@ package com.formulario.bean;
 
 import com.formulario.dao.UsuarioDao;
 import com.formulario.model.Usuario;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.context.RequestContext;
 
 @ManagedBean
 @RequestScoped
@@ -11,6 +14,7 @@ import javax.faces.bean.RequestScoped;
 public class UsuarioBean{
     
     private Usuario usuario= new Usuario();
+    
 
     public Usuario getUsuario() {
         return usuario;
@@ -25,9 +29,12 @@ public class UsuarioBean{
         try{
             dao= new UsuarioDao();
             dao.registrar(usuario);
+            RequestContext.getCurrentInstance().update("growl");
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacion realizada", "Datos Guardados"));
         }catch (Exception e){
             
         }
     }
-    
+  
 }
